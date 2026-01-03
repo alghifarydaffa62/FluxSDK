979df1f333b128d9cba56550f35145a555559b35
M Daffa A <daffa.alghifary62@gmail.com>
2026-01-03 15:03:31 +0700

    feat: decomposing pages

---
+++ app/src/component/VaultDetail/Deposit.tsx
@@ -0,0 +1,9 @@
+
+export default function Deposit() {
+    return(
+        <div className="p-4 bg-yellow-500/10 border border-yellow-500/20 rounded-xl text-yellow-200 text-sm">
+            🚧 <strong>Deposit Component Here</strong> <br/>
+            
+        </div>
+    )
+}
